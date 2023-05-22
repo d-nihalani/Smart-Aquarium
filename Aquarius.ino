be5b6aedc9b50752d2f01749c5ea25484b29f8b2
Drashti Nihalani <92103655+d-nihalani@users.noreply.github.com>
2023-05-22 16:22:05 +0530

    Add files via upload

--- Aquarius.ino
+++ Aquarius.ino
@@ -1,33 +1,38 @@
+//Libraries
 #include <Blynk.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 #include <Servo.h>
 
-
+//initializing pins
 #define LM35_PIN GPIO_NUM_32 
 #define button1_vpin V3 
 #define button2_vpin V4
-#define BLYNK_TEMPLATE_ID "TMPL3AEcug53d"
-#define BLYNK_TEMPLATE_NAME "Aquarium Template"
-#define BLYNK_AUTH_TOKEN "4lxOqqHLnVcCAuc_FABxYR5ak22_l_J3"
+//blynk connection parameters 
+#define BLYNK_TEMPLATE_ID "Your-Template-ID-Here"
+#define BLYNK_TEMPLATE_NAME "Your-Blynk-Template-Name"
+#define BLYNK_AUTH_TOKEN "Your-Blynk-Token-Name"
 #define BLYNK_PRINT Serial
 Servo servo;
 
-char ssid[] = "moto g72_6167";
-char pass[] = "trek5050";
+char ssid[] = "wifi_name";
+char pass[] = "wifi_pwd";
 
+int ledPin = 27;  // Pin connected to the LED
+bool ledState = false;  // Variable that stores state of LED
 
 BlynkTimer timer;
 
-
+// v0 swtich for controlling servo manually
 BLYNK_WRITE(V0)
 {
   int s0 = param.asInt(); 
   servo.write(s0);
-  Blynk.virtualWrite(V1, s0);
+  Blynk.virtualWrite(V1, s0); //v1 switch to display pos of servo
 }
 
+//v2 automatic feed with servo
 BLYNK_WRITE(V2) 
 {
   int pinValue = param.asInt();
@@ -44,7 +49,7 @@ BLYNK_WRITE(V2)
 BLYNK_WRITE(V3) // virtual pin V3 control pump 1
 {
   bool value1 = param.asInt();
-  // Check these values and turn the relay2 ON and OFF
+  // Check these values and turn the relay1 ON and OFF
   if (value1 == 1) {
     digitalWrite(12, HIGH);
   } else {
@@ -67,7 +72,7 @@ void sendTemperature() {
   // Read the analog voltage from LM35 sensor
   float sensorValue = analogRead(LM35_PIN) * (3.3 / 4095);
   // Convert the sensor value to temperature in degrees Celsius
-  float temperature = ((sensorValue) * 100.0)+10; 
+  float temperature = ((sensorValue) * 100.0); 
   // Send temperature value to Blynk app
   Blynk.virtualWrite(V5, temperature);
 }
@@ -77,18 +82,27 @@ void setup()
   Serial.begin(115200);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
   
-  servo.attach(13);
-  servo.write(0);
+  servo.attach(13); //initialize servo input
+  servo.write(0);   //initialize servo pos to 0
   pinMode(12, OUTPUT); // set relay channel 1 to output
   pinMode(14, OUTPUT); // set relay channel 2 to output
   digitalWrite(14,HIGH);
-  digitalWrite(12,HIGH);
+  digitalWrite(12,HIGH);  // set both relay state
+  pinMode(ledPin, OUTPUT);
   
-  timer.setInterval(1000L, sendTemperature); 
+  timer.setInterval(10000, sendTemperature); 
+  //send temp reading after every 10 seconds
 }
 
 void loop() 
 {
   Blynk.run();
   timer.run();
+  bool currentLedState = digitalRead(ledPin);
+
+  // Update virtual button if LED state has changed
+  if (currentLedState != ledState) {
+    ledState = currentLedState;
+    Blynk.virtualWrite(V6, ledState);
+  }
 }
